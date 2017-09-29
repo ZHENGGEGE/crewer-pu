cc71034dbbe9e2e01131d4552ab469a4e5030ed7
ZHENGGEGE <593012736@qq.com>
2017-09-29 15:41:57 +0800

    ..

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ const puppeteer = require('puppeteer');
     await page.goto('https://login.1688.com/member/signin.htm');
     await page.mouse.move(1447, 166);
     await page.mouse.click(1447, 166);
-    await page.type('eelly0921', { delay: 400 })
+    await page.type('***', { delay: 400 }) //账号
     await page.keyboard.down('Tab');
     await page.keyboard.up('Tab');
-    await page.type('123123kk', { delay: 400 })
+    await page.type('***', { delay: 400 })//密码
     await page.mouse.move(1290, 380);
     await page.mouse.click(1290, 380);
     await page.goto('https://s.1688.com/company/company_search.htm?keywords=%CB%D9%C2%F4%CD%A8%B1%AC%BF%EE&n=y&spm=a260k.635.1998096057.d1', { waitUntil: 'networkidle' });
